ec54f2b9dd771f519c184738d44f406b6487af43
OGStan0124 <ogstan0124@gmail.com>
2026-03-15 21:47:38 +0200

    Add player movement and rotation basics

--- main.py
+++ main.py
@@ -11,20 +11,23 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    entities_list = [Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)]
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    clocky = pygame.time.Clock()
+    dt = 0
 
     while(True):
         log_state()
 
-        clocky = pygame.time.Clock()
-        dt = 0
+        screen.fill('black')
+
+        player.update(dt)
+        player.draw(screen)
+
+        pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        for entity in entities_list:
-            entity.draw(screen)
 
         dt = clocky.tick(60) / 1000
 
