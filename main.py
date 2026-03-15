f8c9ca6e764aecfdc0fa0b4c3d34478da019c926
OGStan0124 <ogstan0124@gmail.com>
2026-03-15 19:33:53 +0200

    Added shapes and player object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f'Starting Asteroids with pygame version: {pygame.version.ver}')
@@ -10,13 +11,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    entities_list = [Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)]
+
     while(True):
         log_state()
+
+        clocky = pygame.time.Clock()
+        dt = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
+        for entity in entities_list:
+            entity.draw(screen)
+
+        dt = clocky.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
