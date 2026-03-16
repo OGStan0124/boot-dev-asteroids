9db38eee1cfb1757b5ed2a269f88da3044b4151d
OGStan0124 <ogstan0124@gmail.com>
2026-03-16 17:40:00 +0200

    Add asterids and asteroid fields with basic movement

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f'Starting Asteroids with pygame version: {pygame.version.ver}')
@@ -12,12 +14,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clocky = pygame.time.Clock()
     dt = 0
 
+    asteroid_field = AsteroidField()
 
     while(True):
         log_state()
