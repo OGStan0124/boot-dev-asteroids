6a3fc1f06889e2fbef7c950c5d04d2050eafbcd7
OGStan0124 <ogstan0124@gmail.com>
2026-03-19 13:37:13 +0200

    Shooting an asteroid splits it in 2 if they're big enough

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,4 +12,24 @@ class Asteroid(CircleShape):
         pygame.draw.circle(screen, 'white', self.position, self.radius, LINE_WIDTH)
 
     def update(self, dt):
-        self.position += self.velocity * dt    
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+
+        v1 = self.velocity.rotate(random.uniform(20, 50))
+        v2 = self.velocity.rotate(random.uniform(-50, -20))
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        new_asteroids = [
+            Asteroid(self.position.x, self.position.y, new_radius),
+            Asteroid(self.position.x, self.position.y, new_radius),
+        ]
+
+        new_asteroids[0].velocity = v1 * 1.2
+        new_asteroids[1].velocity = v2 * 1.2
